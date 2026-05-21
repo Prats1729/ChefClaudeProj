3f6ffa6e3bc1310f3379d142b0a98e8301ca5d2f
Prats1729 <prateekverma1729@gmail.com>
2026-05-21 16:02:39 +0530

    implement ingredient list display and update form handling

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,12 +1,21 @@
 import "./main.css";
+import React from "react";
 
 export default function Main() {
+  const [ingredients, setIngredients] = React.useState([])
+
+  const ingredientList = ingredients.map((ingredient) => {
+    return <li key={ingredient}>{ingredient}</li>;
+  })
+
   function handleSubmit(event) {
     event.preventDefault();
-    const formData = new FormData(event.currentTarget);
-    const newIngredient = formData.get("ingredient");
-    console.log(newIngredient);
+    const formData = new FormData(event.currentTarget)
+    const newIngredient = formData.get("ingredient")
+    setIngredients(prevIngredientList => [...prevIngredientList, newIngredient])
+    event.currentTarget.reset()
   }
+
   return (
     <main>
       <form className="main-form" onSubmit={handleSubmit}>
@@ -18,6 +27,20 @@ export default function Main() {
         />
         <button>+ Add Ingredient</button>
       </form>
+      <section className="ingredient-list">
+        {ingredientList.length > 0
+          ? IngredientListDisplay(ingredientList)
+          : null}
+      </section>
     </main>
+  );
+}
+
+function IngredientListDisplay(ingredientList) {
+  return (
+    <>
+      <h2>Ingredients on hand:</h2>
+      <ul>{ingredientList}</ul>
+    </>
   );
 }
