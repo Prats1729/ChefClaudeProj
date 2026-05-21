40a9eaf4eac7e41b16dea1bdf9d02f53aa8714d8
Prats1729 <prateekverma1729@gmail.com>
2026-05-21 19:58:03 +0530

    fix: add aria-live attribute for accessibility in AiRecipe component

--- src/components/recipe.jsx
+++ src/components/recipe.jsx
@@ -4,7 +4,7 @@ import './recipe.css'
 
 export default function AiRecipe(props){
     return (
-      <section className="ai-recipe">
+      <section className="ai-recipe" aria-live='polite'>
         <h2>Recipe Suggested by Chef Claude :</h2>
         <Markdown>{props.recipe}</Markdown>
       </section>
